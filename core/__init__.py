1f2574c025d387d0fa5a3d571befdd402f0e2cf5
youandme191298 <youandme191298@gmail.com>
2025-10-20 02:59:51 +0700

    __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -1,1 +1,13 @@
+# core/__init__.py
+import importlib, os
+
+__all__ = []
+
+for file in os.listdir(os.path.dirname(__file__)):
+    if file.endswith(".py") and not file.startswith("__"):
+        module_name = file[:-3]
+        importlib.import_module(f"core.{module_name}")
+        __all__.append(module_name)
+
+print(f"[CORE] 🧩 Đã nạp {len(__all__)} tầng năng lượng: {__all__}", flush=True)
 
